ef060c6793d62c7b11bd2355488e7a3c5a63a717
mmzyk <mmzyk@opscode.com>
2015-05-13 09:54:33 -0400

    Add in code to check new ace vs old ace; currently a no-op
    This commit adds in the code to retrieve the current ace
    and compare it against the new ace to see if the admin group
    has been removed. The code is executed in the normal flow, but
    is currently a no-op as it only returns ok in all cases, so
    behavior is not modified.

--- src/oc_erchef/apps/oc_chef_authz/src/oc_chef_authz_acl_constraints.erl
+++ src/oc_erchef/apps/oc_chef_authz/src/oc_chef_authz_acl_constraints.erl
@@ -10,16 +10,50 @@
 check_acl_constraints(AuthzId, Type, AclPerm, Ace) ->
   check_admin_group_removal_from_grant_ace(AuthzId, Type, AclPerm, Ace).
 
-check_admin_group_removal_from_grant_ace(AuthzId, Type, AclPerm, Ace) ->
+check_admin_group_removal_from_grant_ace(AuthzId, Type, AclPerm, NewAce) ->
   case AclPerm of
     <<"grant">> ->
-      ActorsAndGroups = ej:get({AclPerm}, Ace),
-      _Groups = ej:get({<<"groups">>}, ActorsAndGroups),
-      _ExistingAcl = oc_chef_authz_acl:fetch(Type, AuthzId),
-      ok;
+      check_admin_group_removal(AuthzId, Type, AclPerm, NewAce);
     _Other ->
       ok
   end.
 
+check_admin_group_removal(AuthzId, Type, AclPerm, NewAce) ->
+      NewGroups = extract_acl_groups(AclPerm, NewAce),
+      CurrentAce = oc_chef_authz_acl:fetch(Type, AuthzId),
+      CurrentGroups = extract_acl_groups(AclPerm, CurrentAce),
+
+      %% Check if the CurrentGroups contains the admin group -
+      %% if it doesn't, there is nothing to do. If it does,
+      %% then check if the admin group is present in the
+      %% new group. If it isn't, fail.
+      %% This will have the effect that if a group doesn't have the
+      %% admin group on the grant ace, but then it is later added, that admin group
+      %% will never be able to be removed.
+      %% Should we restrict this check to only the grant aces of certain objects
+      %% to prevent this being an issue?
+      case contains_admin_group(CurrentGroups) of
+        false ->
+          ok;
+        true ->
+          case contains_admin_group(NewGroups) of
+            true ->
+              ok;
+            false ->
+              ok %% Actually, not okay, need to return something else
+          end
+      end.
+
+contains_admin_group(Groups) ->
+  case lists:filter(fun(X) -> X =:= <<"admins">> end, Groups) of
+    [] ->
+        false;
+    _NonEmpty ->
+        true
+    end.
+
+extract_acl_groups(AclPerm, Ace) ->
+      ActorsAndGroups = ej:get({AclPerm}, Ace),
+      ej:get({<<"groups">>}, ActorsAndGroups).
 
 
